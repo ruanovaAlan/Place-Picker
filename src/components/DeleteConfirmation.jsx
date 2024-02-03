a893eb7ffb56655a7c4c9c7d70523425c153aba1
Alan Ruanova <ruanovaalan96@gmail.com>
2024-02-02 19:25:54 -0600

    Add timer to automatically confirm delete after 3 seconds

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    // cleanup
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
